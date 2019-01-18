1b81f1cc0923e35b3ed5f3595d16546be362d734
dani <dani.zaghian@gmail.com>
2019-01-18 11:23:43 -0800

    adds url segment params

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@ Rails.application.routes.draw do
   # verb "/urls" => "namespace/controllers#action"
   namespace :api do
   	get "/query_params_url" => "params_examples#query_params_method"
+
   	get "/name_url" => "params_examples#name_method"
+
   	get "/guess_query_url" => "params_examples#guess_query_method"
+
   	get "/segment_params_url/:purple_hippo" => "params_examples#segment_params_method"
+
+  	get "/guess_segment/:guess" => "params_examples#guess_query_method"
+
   end
 end
